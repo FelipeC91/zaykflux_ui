9914ec7726db05a95ba3646e7945448caf49dde2
FelipeC91 <feelipecampos91@gmail.com>
2023-10-17 00:20:42 -0300

    first commit

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,10 @@ import { CommonModule } from '@angular/common';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 import { ButtonModule } from 'primeng/button';
+import { SidebarModule } from 'primeng/sidebar';
+
+import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 @NgModule({
   declarations: [
@@ -11,10 +15,20 @@ import { ButtonModule } from 'primeng/button';
   ],
   imports: [
     CommonModule,
-    ButtonModule
+    ButtonModule,
+    SidebarModule,
+    
+    FontAwesomeModule
+
   ],
   exports: [
     NavBarComponent
   ]
 })
-export class CoreModule { }
+export class CoreModule { 
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faBars)
+  }
+  
+}
--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-nav-bar',
   templateUrl: './nav-bar.component.html',
-  styleUrls: ['./nav-bar.component.scss']
+  styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
-  private sideBarVisibility = false;
 
-  public showSideBar(): void {
-      this.sideBarVisibility = true;
-  }
+  showSideBar = false;
+
 
 }
