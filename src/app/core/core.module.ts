46606b689f8650a62aaf3ff062fc8eb54799032f
FelipeC91 <feelipecampos91@gmail.com>
2023-10-22 23:15:46 -0300

    reorganize scss

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -7,6 +7,7 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { ButtonModule } from 'primeng/button';
 import { SidebarModule } from 'primeng/sidebar';
 import { InputTextModule } from 'primeng/inputtext';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
 
 import { FontAwesomeModule, FaIconLibrary } from '@fortawesome/angular-fontawesome';
 import { faBars, faBell, faMagnifyingGlass, faPlus, faChartLine, faTicket, faUserTie, faChartSimple, faBookOpenReader, faIdCard, faGears, faTicketSimple } from '@fortawesome/free-solid-svg-icons';
@@ -22,6 +23,7 @@ import { faBars, faBell, faMagnifyingGlass, faPlus, faChartLine, faTicket, faUse
     ButtonModule,
     SidebarModule,
     InputTextModule,
+    OverlayPanelModule,
     
     FontAwesomeModule
 
--- src/app/core/nav-bar/nav-bar.component.ts
+++ src/app/core/nav-bar/nav-bar.component.ts
@@ -32,4 +32,5 @@ export class NavBarComponent {
     this.toggleTicketSearch(ticketNumberInput)
   }
 
+
 }
